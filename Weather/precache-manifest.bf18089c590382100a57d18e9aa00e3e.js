55c576578090b3e05058a8e96d10050cc8de6986
vadim <diklineage4@gmail.com>
2019-06-22 02:29:58 +0300

    add project Weather

---
+++ Weather/precache-manifest.bf18089c590382100a57d18e9aa00e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "76cb13a70e7bc9613818",
+    "url": "/static/js/main.76cb13a7.chunk.js"
+  },
+  {
+    "revision": "ac6639dce616533a949b",
+    "url": "/static/js/2.ac6639dc.chunk.js"
+  },
+  {
+    "revision": "76cb13a70e7bc9613818",
+    "url": "/static/css/main.0b207f80.chunk.css"
+  },
+  {
+    "revision": "931d054a77a92bdce432633dca13d907",
+    "url": "/index.html"
+  }
+];
