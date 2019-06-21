792fd1bc1749a7487f9050dd10ce9e37fff27e0e
kydryashov <vadimkudryashov.ibrush@gmail.com>
2019-06-22 01:54:06 +0300

    Add project

---
+++ precache-manifest.fdcf3f74a71468ebc0f2f5f3e451a275.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b435128f4aa5d3f6202e35a339643c0",
+    "url": "./static/media/Exo20-Thin.7b435128.woff2"
+  },
+  {
+    "revision": "715d73a57914b6c2f309",
+    "url": "./static/css/main.8c196438.chunk.css"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "./static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6f47a89bdb6eb676e6d049309ece6718",
+    "url": "./static/media/cat.6f47a89b.png"
+  },
+  {
+    "revision": "715d73a57914b6c2f309",
+    "url": "./static/js/main.715d73a5.chunk.js"
+  },
+  {
+    "revision": "b7f2073db20fbe66e67c7b28189c9f3b",
+    "url": "./static/media/Exo20-Thin.b7f2073d.woff"
+  },
+  {
+    "revision": "f782a444c9877f04dce834988e8838cb",
+    "url": "./static/media/pattern.f782a444.png"
+  },
+  {
+    "revision": "af1d9a90239a3d6b8e107072521da874",
+    "url": "./static/media/TrebuchetMS.af1d9a90.woff2"
+  },
+  {
+    "revision": "f2bb6f2202822c63c05ffbcfb9cdbb54",
+    "url": "./static/media/TrebuchetMS.f2bb6f22.woff"
+  },
+  {
+    "revision": "e3731fd32930b6727bc7f877df08169e",
+    "url": "./index.html"
+  }
+];
