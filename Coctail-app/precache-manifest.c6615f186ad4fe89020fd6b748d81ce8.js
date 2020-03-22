6fd9fdd8a0c4762c2fcc060d9068e7e58afdfcf2
Vadim Kudryashov <diklineage4@gmail.com>
2020-03-23 00:35:01 +0300

    upd

---
+++ Coctail-app/precache-manifest.c6615f186ad4fe89020fd6b748d81ce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b4fde525e421c9da8d1858eee4f6ce",
+    "url": "/Coctail-app/index.html/index.html"
+  },
+  {
+    "revision": "b8f3ab1209bdddbd6a8f",
+    "url": "/Coctail-app/index.html/static/css/main.4f89aa5b.chunk.css"
+  },
+  {
+    "revision": "b004e6636cc3f9079b8b",
+    "url": "/Coctail-app/index.html/static/js/2.953f9158.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/Coctail-app/index.html/static/js/2.953f9158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f3ab1209bdddbd6a8f",
+    "url": "/Coctail-app/index.html/static/js/main.413ab0ec.chunk.js"
+  },
+  {
+    "revision": "9b8a3f0b1c07580c6c06",
+    "url": "/Coctail-app/index.html/static/js/runtime-main.99194ffb.js"
+  },
+  {
+    "revision": "a94eb3b92e312562b2f71d225813669e",
+    "url": "/Coctail-app/index.html/static/media/basket.a94eb3b9.svg"
+  }
+]);
--- Coctail-app/service-worker.js
+++ Coctail-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.be168deb929bd76a1f79abebc00562d9.js"
+  "/Coctail-app/index.html/precache-manifest.c6615f186ad4fe89020fd6b748d81ce8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Coctail-app/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
