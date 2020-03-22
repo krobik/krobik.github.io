0deb5d56d13f0d60b885b19d2f307b7d62618130
Vadim Kudryashov <diklineage4@gmail.com>
2020-03-23 00:08:25 +0300

    ref

---
+++ Coctail-app/precache-manifest.263c146e611fcfa8d35a8c0e0d20b0be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195b8b6b6a2c7c9733815a48389f9b2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41e94f5bf00545fb9e31",
+    "url": "/static/css/main.4f89aa5b.chunk.css"
+  },
+  {
+    "revision": "4a67cf351c9fb289c0d9",
+    "url": "/static/js/2.ef1367d1.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.ef1367d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e94f5bf00545fb9e31",
+    "url": "/static/js/main.e4e846e7.chunk.js"
+  },
+  {
+    "revision": "30967c40040320d0b91a",
+    "url": "/static/js/runtime-main.021e82a6.js"
+  },
+  {
+    "revision": "a94eb3b92e312562b2f71d225813669e",
+    "url": "/static/media/basket.a94eb3b9.svg"
+  }
+]);
--- Coctail-app/service-worker.js
+++ Coctail-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.267c58757e594bf0b8aa216457597a02.js"
+  "/precache-manifest.263c146e611fcfa8d35a8c0e0d20b0be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
