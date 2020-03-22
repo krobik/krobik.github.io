a2e99c76717dca6ca1b4d631088c5bf53f3afe1e
Vadim Kudryashov <diklineage4@gmail.com>
2020-03-23 00:44:04 +0300

    upd

---
+++ Coctail-app/precache-manifest.598656eea5dfd9fe02589485115ba0f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba25b7d1784e079f921f55128a498aa2",
+    "url": "/Coctail-app/index.html/index.html"
+  },
+  {
+    "revision": "f87d52d39ed002ca0d63",
+    "url": "/Coctail-app/index.html/static/css/main.4f89aa5b.chunk.css"
+  },
+  {
+    "revision": "b004e6636cc3f9079b8b",
+    "url": "/Coctail-app/index.html/static/js/2.953f9158.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/Coctail-app/index.html/static/js/2.953f9158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87d52d39ed002ca0d63",
+    "url": "/Coctail-app/index.html/static/js/main.2f6afd9f.chunk.js"
+  },
+  {
+    "revision": "9b8a3f0b1c07580c6c06",
+    "url": "/Coctail-app/index.html/static/js/runtime-main.99194ffb.js"
+  },
+  {
+    "revision": "a94eb3b92e312562b2f71d225813669e",
+    "url": "/Coctail-app/index.html/static/media/basket.a94eb3b9.svg"
+  }
+]);
