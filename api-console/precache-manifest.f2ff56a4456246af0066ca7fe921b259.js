b03093ac31cc405cd07500a5fb5bb94b4a0a97a8
Vadim Kudryashov <diklineage4@gmail.com>
2020-06-21 22:05:49 +0300

    update homapage

---
+++ api-console/precache-manifest.f2ff56a4456246af0066ca7fe921b259.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fea1ae437a91a42d7cdf00497e611a4",
+    "url": "/api-console/index.html"
+  },
+  {
+    "revision": "07e07399dabbc7f58266",
+    "url": "/api-console/static/css/main.a1dc9b43.chunk.css"
+  },
+  {
+    "revision": "c50f4eabfdb687a536ad",
+    "url": "/api-console/static/js/2.c03f0268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/api-console/static/js/2.c03f0268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e07399dabbc7f58266",
+    "url": "/api-console/static/js/main.cd4f3d5e.chunk.js"
+  },
+  {
+    "revision": "b96f143216f33965ae95",
+    "url": "/api-console/static/js/runtime-main.7105ab00.js"
+  },
+  {
+    "revision": "1b433a821ef8f9a70eac6aea64f7cac8",
+    "url": "/api-console/static/media/FiraCode-Regular.1b433a82.ttf"
+  },
+  {
+    "revision": "38809fcb9cf86f6aa7b99b699b41dae1",
+    "url": "/api-console/static/media/FiraCode-Regular.38809fcb.eot"
+  },
+  {
+    "revision": "7530064a796a548c000e0c675e839112",
+    "url": "/api-console/static/media/FiraCode-Regular.7530064a.woff2"
+  },
+  {
+    "revision": "843b60457048ffd1818953d593d06557",
+    "url": "/api-console/static/media/FiraCode-Regular.843b6045.woff"
+  },
+  {
+    "revision": "06b45761bfabccf92c66efc6e3d6fbec",
+    "url": "/api-console/static/media/SFProText-Regular.06b45761.woff"
+  },
+  {
+    "revision": "c7e2854dfcedf02e21c78e34bd7a7141",
+    "url": "/api-console/static/media/SFProText-Regular.c7e2854d.woff2"
+  },
+  {
+    "revision": "fbc586bf988c38ae312760a21e8c9bf5",
+    "url": "/api-console/static/media/clear.fbc586bf.svg"
+  },
+  {
+    "revision": "a3c29d72ba186e7f76261c6885d1a4cd",
+    "url": "/api-console/static/media/exit-full-screen.a3c29d72.svg"
+  },
+  {
+    "revision": "e40c208f2d97c931f2056af5e3122673",
+    "url": "/api-console/static/media/format.e40c208f.svg"
+  },
+  {
+    "revision": "620c546f1b5d3a66b71ca826a9f07b25",
+    "url": "/api-console/static/media/log-out.620c546f.svg"
+  },
+  {
+    "revision": "fa4fd57fecf0e30fc8057cc56e3c26a9",
+    "url": "/api-console/static/media/logo.fa4fd57f.svg"
+  },
+  {
+    "revision": "07f13d821ae6562ed5deb3678c8cfba6",
+    "url": "/api-console/static/media/meh.07f13d82.svg"
+  },
+  {
+    "revision": "e840f96b3150c525aded908bd9256b8e",
+    "url": "/api-console/static/media/to-full-screen.e840f96b.svg"
+  }
+]);
--- api-console/service-worker.js
+++ api-console/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a439dac096cf76779f2b687fec56fc5d.js"
+  "/api-console/precache-manifest.f2ff56a4456246af0066ca7fe921b259.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/api-console/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
