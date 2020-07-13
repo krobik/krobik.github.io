bc2ed04f17e8f1df94f35993d27b9d48bbfe2d62
Vadim Kudryashov <diklineage4@gmail.com>
2020-07-13 11:54:23 +0300

    add perx

---
+++ perx/precache-manifest.6b6b428bf5f1863134fc324522bf4e6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e4956dd29a13e4deed1fe0c34e2923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b22af8e95dbe4bc4ed",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "a8fba42112a8f26826e6",
+    "url": "/static/css/main.ec3cb4fc.chunk.css"
+  },
+  {
+    "revision": "51b22af8e95dbe4bc4ed",
+    "url": "/static/js/2.6823e5de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6823e5de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8fba42112a8f26826e6",
+    "url": "/static/js/main.d530060e.chunk.js"
+  },
+  {
+    "revision": "3582da37c4b7e491b2dc",
+    "url": "/static/js/runtime-main.fc427433.js"
+  }
+]);
