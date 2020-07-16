fde3afba56c09be85ff8271804736159438816df
Vadim Kudryashov <diklineage4@gmail.com>
2020-07-16 18:38:10 +0300

    add project vending machine

---
+++ vending-machine/precache-manifest.c06fe3360d947f84dec1a69e1cef9ee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f301c5a15ed508ca3d48fd8f0b8a25cc",
+    "url": "/vending-machine/index.html"
+  },
+  {
+    "revision": "08f2e29a6076653496e0",
+    "url": "/vending-machine/static/css/main.9ef36311.chunk.css"
+  },
+  {
+    "revision": "7db3873abfd730114158",
+    "url": "/vending-machine/static/js/2.9af22527.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vending-machine/static/js/2.9af22527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f2e29a6076653496e0",
+    "url": "/vending-machine/static/js/main.096f52fc.chunk.js"
+  },
+  {
+    "revision": "da6bf439b721347829a2",
+    "url": "/vending-machine/static/js/runtime-main.fb871aa5.js"
+  },
+  {
+    "revision": "349eca0820eda29c4ad9502046e38c0a",
+    "url": "/vending-machine/static/media/triangle.349eca08.svg"
+  }
+]);
